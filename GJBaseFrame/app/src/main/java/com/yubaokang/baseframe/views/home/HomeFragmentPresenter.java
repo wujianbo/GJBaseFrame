d68a16f5f3a903fdd57b5a281efee51bb0c1cbdc
yubaokang <ybk@goujiawang.com>
2016-09-08 17:46:18 +0800

    rxjava2

--- GJBaseFrame/app/src/main/java/com/yubaokang/baseframe/views/home/HomeFragmentPresenter.java
+++ GJBaseFrame/app/src/main/java/com/yubaokang/baseframe/views/home/HomeFragmentPresenter.java
@@ -1,6 +1,5 @@
 package com.yubaokang.baseframe.views.home;
 
-import com.gj.base.lib.utils.L;
 import com.yubaokang.baseframe.base.dagger.app.App;
 import com.yubaokang.baseframe.base.dagger.scopes.ActivityScope;
 import com.yubaokang.baseframe.model.response.WeiXinDataListRes;
@@ -9,10 +8,13 @@ import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Flowable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subscribers.DefaultSubscriber;
 
 
 /**
@@ -48,33 +50,60 @@ public class HomeFragmentPresenter implements HomeFragmentContract.Presenter {
                 .flatMap(new Function<WeiXinDataListRes, Publisher<WeiXinDataListRes>>() {
                     @Override
                     public Publisher<WeiXinDataListRes> apply(WeiXinDataListRes weiXinDataListRes) throws Exception {
-                        weiXinDataListRes = null;
                         if (weiXinDataListRes == null) {
                             return Flowable.error(new NullPointerException("解析错误"));
                         }
                         return Flowable.just(weiXinDataListRes);
                     }
                 })
-                .subscribe(new Subscriber<WeiXinDataListRes>() {
+                .timeout(1000, TimeUnit.SECONDS, new Flowable<WeiXinDataListRes>() {
+                    @Override
+                    protected void subscribeActual(Subscriber<? super WeiXinDataListRes> s) {
+                        s.onError(new Throwable());
+                    }
+                })
+                .switchMap(new Function<WeiXinDataListRes, Publisher<?>>() {
+                    @Override
+                    public Publisher<?> apply(WeiXinDataListRes weiXinDataListRes) throws Exception {
+                        return null;
+                    }
+                })
+                .subscribe(new DefaultSubscriber<Object>() {
+                    @Override
+                    public void onNext(Object o) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+
+                    }
+
                     @Override
                     public void onComplete() {
-                        L.i("哈哈哈");
-                    }
 
-                    @Override
-                    public void onError(Throwable e) {
-                        view.showEmpty();
-                    }
-
-                    @Override
-                    public void onSubscribe(Subscription s) {
-                        subscription = s;
-                    }
-
-                    @Override
-                    public void onNext(WeiXinDataListRes weiXinDataListRes) {
-                        view.showDatas(weiXinDataListRes);
                     }
                 });
+//                .subscribe(new Subscriber<WeiXinDataListRes>() {
+//                    @Override
+//                    public void onComplete() {
+//                        L.i("哈哈哈");
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//                        view.showEmpty();
+//                    }
+//
+//                    @Override
+//                    public void onSubscribe(Subscription s) {
+//                        subscription = s;
+//                    }
+//
+//                    @Override
+//                    public void onNext(WeiXinDataListRes weiXinDataListRes) {
+//                        view.showDatas(weiXinDataListRes);
+//                    }
+//                });
     }
 }
